af17d2c732841a7920114ec7006cfa8f5aa9fc2d
Olivia Fuentes <fuentesolivia45@gmail.com>
2026-04-21 22:18:54 -0400

    Collisions

--- Program.cs
+++ Program.cs
@@ -37,6 +37,35 @@ class Program
                 gapY = rand.Next(5, 15);
                 score++;
             }
+            if (birdY < 0 || birdY > 20)
+                break;
+
+            if (pipeX == 5 && (birdY < gapY || birdY > gapY + 4))
+                break;
+
+            Console.Clear();
+
+            Console.SetCursorPosition(5, birdY);
+            Console.Write("O");
+
+            for (int i = 0; i < gapY; i++)
+            {
+                Console.SetCursorPosition(pipeX, i);
+                Console.Write("|");
+            }
+
+
+            for (int i = gapY + 5; i < 25; i++)
+            {
+                Console.SetCursorPosition(pipeX, i);
+                Console.Write("|");
+
+            }
+            Console.SetCursorPosition(0, 0);
+            Console.Write("Score: " + score);
+            Thread.Sleep(100);
+
+
         }
     }
 }
